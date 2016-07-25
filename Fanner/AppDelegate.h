203ac2d49ef5d032d5a344503aaaf0e01f103be0
GangsterMy <impzone@163.com>
2016-07-25 15:04:22 +0800

    First

---
+++ Fanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fanner
+//
+//  Created by 赵麦 on 7/25/16.
+//  Copyright © 2016 歹徒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fanner
+//
+//  Created by 赵麦 on 7/25/16.
+//  Copyright © 2016 歹徒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
